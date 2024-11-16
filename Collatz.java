a2e9035ffb5f902171e39272b6da80c619366658
Or Sintov <sintov@Ors-Laptop.local>
2024-11-17 00:44:46 +0200

    push Collatz first time

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,34 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int highestSeed = Integer.parseInt(args[0]);
+		String mood = args[1];
+		int successCounter = 0;
+		for (int i = 1; i <= highestSeed; i++){
+			int counter = 1;
+			int collatzNum = i;
+			if (mood.equals("v")){
+				System.out.print(collatzNum + " ");
+			}
+			do{
+				if (collatzNum % 2 == 0){
+					collatzNum = collatzNum / 2;
+				} else{
+					collatzNum = (collatzNum * 3) + 1;
+				}
+				if (mood.equals("v")){
+					System.out.print(collatzNum + " ");
+				}
+				counter ++;
+			}while (collatzNum != 1);
+			if (mood.equals("v")){
+				System.out.println("(" + counter + ")");
+			}
+			successCounter ++;
+		}
+		System.out.println("Every one of the first " + successCounter +
+							 " hailstone sequences reached 1");
+
+
 	}
 }
