c582fa79a021d92376b82c631990d188c92c575c
Or Sintov <sintov@Ors-Laptop.local>
2024-11-16 12:05:50 +0200

    push Cheers third time

--- Cheers.java
+++ Cheers.java
@@ -3,10 +3,12 @@ public class Cheers {
         public static void main(String[] args) {
             String anLetters = "AEFHILNMNOR";
             String crowdWord = args[0].toUpperCase();
+            System.out.println(crowdWord);
             int numberOfTimes = Integer.parseInt(args[1]);
             for (int i = 0; i < crowdWord.length(); i++){
                 char letter = crowdWord.charAt(i);
-                if (anLetters.indexOf(letter) > 0){
+                //System.out.println(anLetters.indexOf(letter));
+                if (anLetters.indexOf(letter) >= 0){
                         System.out.println("Give me an " + letter + ": " + letter + "!");        
                 } else {
                         System.out.println("Give me a  " + letter + ": " + letter + "!");
