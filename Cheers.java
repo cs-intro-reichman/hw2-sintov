4245a5f35aca2c6559ab961d20421fe1c4fa883f
Or Sintov <sintov@Ors-Laptop.local>
2024-11-16 12:12:13 +0200

    push Cheers fourth time

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,12 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-            String anLetters = "AEFHILNMNOR";
+            String anLetters = "AEFHILNMNORSX";
             String crowdWord = args[0].toUpperCase();
             System.out.println(crowdWord);
             int numberOfTimes = Integer.parseInt(args[1]);
             for (int i = 0; i < crowdWord.length(); i++){
                 char letter = crowdWord.charAt(i);
-                //System.out.println(anLetters.indexOf(letter));
                 if (anLetters.indexOf(letter) >= 0){
                         System.out.println("Give me an " + letter + ": " + letter + "!");        
                 } else {
